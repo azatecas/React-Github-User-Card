4f352ad0b0ef1a96b3f533c09cb4bd41e94e10a1
Luis PM <luisp237@gmail.com>
2020-02-12 09:17:33 -0500

    added card component that displays data from github api

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component }from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Card from './components/Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      user: []
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/azatecas')
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          user: res.data
+        })
+      })
+      .catch(err => console.log("something is not right", err));      
+  }
+
+ render(){
+    return (
+      <div className="App">
+        <Card 
+            info={this.state.user}
+        />
+      
+      </div>
+    );
+  } 
 }
 
 export default App;
---
+++ github-user-card/src/components/Card.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../App.css';
+
+const Card = (props) => {
+    console.log('card',props.info.login)
+    return (
+        <div className="user-card">
+            <div >
+                <img className="user-img" src={props.info.avatar_url} alt={`profile-img-of-${props.info.name}`}/>
+            </div>
+            <div>
+                <h1>{ props.info.login }</h1>
+                <p>Name: { props.info.name }</p>
+                <p>Location: { props.info.location }</p>
+                <p>Followers: { props.info.followers }</p>
+                <p>Profile: <a href={ props.info.html_url }>{ props.info.html_url }</a></p>
+            </div>
+            
+        </div>
+    )
+
+}
+
+export default Card;
